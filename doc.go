6eebed8a86bb20e85c66a0248ef3b1bc35787385
Nicholas Ng <nickng@projectfate.org>
2016-10-15 22:55:07 +0100

    Add header

--- doc.go
+++ doc.go
@@ -1,4 +1,18 @@
+// Copyright © 2016 Nicholas Ng <nickng@projectfate.org>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 // Package cfsm is a library for working with Communicating Finite State
 // Machines.
 //
-package cfsm
+package cfsm // import "github.com/nickng/cfsm"
